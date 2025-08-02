f770832cc5e359da66a597339fccb6b8fdd54f1f
Pratik maru <pratikmaru80762@example.com>
2025-08-02 14:55:21 +0530

    set up dev and prod env

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 dotenv.config();
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.URI);
+  await mongoose.connect(process.env.MONGO_URI);
 };
 
 export default connectDB;
